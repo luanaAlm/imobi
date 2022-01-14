ecb9e87b28052cf4318d0ab21ef5885a2e3b5216
Luana Rodirgues <luanarodrigues3211@gmail.com>
2022-01-14 15:43:53 -0300

    Criação da home na views.py

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='/auth/logar/')
+def home(request):
+    return HttpResponse('home')
